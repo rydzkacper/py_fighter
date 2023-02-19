54881b5ecf55cf58ba083da00792107db5afa775
rydzkacper <rydzewskikacperr@gmail.com>
2023-02-19 18:57:10 +0100

    added players and fps cap

--- fighter.py
+++ fighter.py
@@ -2,3 +2,19 @@ import pygame
 class Fighter():
     def __init__(self, x, y):
         self.rect = pygame.Rect((x, y, 80, 180))
+    def move(self):
+        SPEED = 10
+        dx = 0
+        dy = 0
+
+        #keypresses
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a]:
+            dx = - SPEED
+        if key[pygame.K_d]:
+            dx =  SPEED
+        self.rect.x += dx
+        self.rect.y += dy
+    def draw(self, surface):
+        pygame.draw.rect(surface,(255, 0, 0), self.rect)
--- main.py
+++ main.py
@@ -7,7 +7,9 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("PY FIGHTER")
-
+#framerate
+clock = pygame.time.Clock()
+FPS = 60
 # background image
 bg_image = pygame.image.load("assets/images/backgrounds/background.jpeg")
 
@@ -16,12 +18,20 @@ def draw_bg():
     screen.blit(scale_bg, (0,0))
 
 # instances of fighters
-
+fighter_1 = Fighter(200, 310)
+fighter_2 = Fighter(700, 310)
 # game loop
 run = True
 while run:
+    clock.tick(FPS)
     #background
     draw_bg()
+    #fighters move
+    fighter_1.move()
+    fighter_2.move()
+    #fighters draw
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
